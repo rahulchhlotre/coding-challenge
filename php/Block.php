62a3ab4ec8c13881147ebe2e5910dd26f00e14c9
rahulchhlotre <rahulchhlotre@gmail.com>
2021-12-23 18:04:36 +0530

    Changes in render_callback for security , performance and coding standard

--- php/Block.php
+++ php/Block.php
@@ -72,29 +72,28 @@ class Block {
 		$currentpost = intval($_GET['post_id']);
 		if (! $currentpost ) {
 			$currentpost = '';
-		} ?>
+		}
 
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
-			<ul>
-				<?php foreach ( $post_types as $post_type_slug ) :
-	                $post_type_object = get_post_type_object( $post_type_slug  );
-	                $post_count = wp_count_posts($post_type_slug); 
-	                ?>
-					<li><?php echo 'There are ' . $post_count . ' ' .
-						  $post_type_object->labels->name . '.'; ?></li>
-				<?php endforeach;	?>
+       		 echo '<div class="'.$class_name.'">';
+		 	echo '<h2>Post Counts</h2>';
+		 	echo '<ul>';
+			foreach ( $post_types as $post_type_slug ) :
+                $post_type_object = get_post_type_object( $post_type_slug  );
+                $post_count = wp_count_posts($post_type_slug); 
+                
+				 echo '<li>There are ' . $post_count . ' ' .
+					  $post_type_object->labels->name . '.</li>';
+			endforeach;
 
-			</ul>
-			<p><?php echo 'The current post ID is ' . $currentpost . '.'; ?></p>
+			echo '</ul>';
+			echo '<p>The current post ID is ' . $currentpost . '.</p>'; 
 
-			<?php
 			$query = new WP_Query(  array(
 				'post_type' => ['post', 'page'],
 				'posts_per_page' => 6,
 				'post_status' => 'any',
 				'tag'  => 'foo',
-                'category_name'  => 'baz',
+                		'category_name'  => 'baz',
 				'date_query' => array(
 					array(
 						'hour'      => 9,
@@ -108,17 +107,18 @@ class Block {
 			));
 
 			if ($query->found_posts ) : 
-				$posts = 0; ?>
-				<h2>5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                	<?php while ( $query->have_posts() && $posts < 5 ) { 
-                		if (get_the_ID() != $currentpost ){ $posts++; ?>
-                    		<li><?php echo get_the_title(); ?></li>
-                	<?php } } 
-                	wp_reset_postdata(); ?>
-                </ul>
-			<?php endif; ?>
+				$posts = 0;
+				echo '<h2>5 posts with the tag of foo and the category of baz</h2>';
+				echo '<ul>';
+				 while ( $query->have_posts() && $posts < 5 ) { 
+					if (get_the_ID() != $currentpost ){ $posts++; 
+						the_title( '<li>', '</li>');
+					} 
+				} 
+				echo '</ul>';
+                		wp_reset_postdata(); 
+		 	endif; 
 			
-		</div>
-		<?php return ob_get_clean();
+		echo '</div>';
+		return ob_get_clean();
 	}
